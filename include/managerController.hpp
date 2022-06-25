bc757c41f0089125d41178f553fd5bb9ff74fc9b
Pedro Dias <pedrodias504@gmail.com>
2022-06-25 14:20:36 -0300

    Implemening the manager controller

--- include/managerController.hpp
+++ include/managerController.hpp
@@ -6,28 +6,26 @@
 
 using std::string, std::vector;
 
+struct InventoryEntry {
+    InventoryEntry(string name, int product_amount, double product_price) :
+        product_name(name), amount(product_amount), price(product_price) {
+
+    }
+
+    string product_name;
+    int amount;
+    double price;
+};
+
 class ManagerController{
     public:
-        static ManagerController getById(DatabaseConnection& db_connection, int id);
-        // Returns the first product matching the given name on the database
-        static ManagerController getByName(DatabaseConnection& db_connection, string name);
-        static ManagerController createProduct(DatabaseConnection& db_connection,
-            string name, double price, int amount);
-        static void removeProduct(DatabaseConnection& db_connection, int id);
-		int id();
-        string name();
-        double price();
-        int amount();
-        void updatePrice(double new_price);
-        void updateAmount(int new_amount);
-        //void checkStock();
+        ManagerController(DatabaseConnection& db_connection);
+        void createProduct(const InventoryEntry& new_entry);
+        void updatePrice(string product_name, double new_price);
+        void updateInventory(string product_name, int new_amount);
+        void removeProduct(string product_name);
+        vector<InventoryEntry> checkInventory();
     private:
-		ManagerController(int id, string name, double price, int amount,
-            DatabaseConnection& db_connection);
-
-        int id_, amount_;
-        string name_;
-        double price_;
         DatabaseConnection& db_connection_;
 };
 
--- source/managerController.cpp
+++ source/managerController.cpp
@@ -1,119 +1,39 @@
 #include "managerController.hpp"
 #include <stdexcept>
+#include "productModel.hpp"
 
-using std::runtime_error, std::string;
+using std::runtime_error;
 
-ManagerController::ManagerController(
-int id,
-string name,
-double price,
-int amount,
-DatabaseConnection& db_connection
-) : id_(id), amount_(amount), name_(name), price_(price), db_connection_(db_connection) { }
+ManagerController::ManagerController(DatabaseConnection& db_connection)
+    : db_connection_(db_connection) {
 
-ManagerController ManagerController::getById(DatabaseConnection& db_connection, int id) {
-    string sql_statement = db_connection.
-        prepareStatement("SELECT * FROM Prodcuts WHERE id=?", "i", id);
-    QueryResults result = db_connection.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to retrieve product with id=" + std::to_string(id) + "!\nDatabase error encountered: " + result.status_message());
+}
+
+void ManagerController::createProduct(const InventoryEntry& new_entry) {
+    ProductModel::createProduct(db_connection_, new_entry.product_name,
+        new_entry.price, new_entry.amount);
+}
+
+void ManagerController::updatePrice(string product_name, double new_price) {
+    ProductModel target_product = ProductModel::getByName(db_connection_, product_name);
+    target_product.updatePrice(new_price);
+}
+
+void ManagerController::updateInventory(string product_name, int new_amount) {
+    ProductModel target_product = ProductModel::getByName(db_connection_, product_name);
+    target_product.updateAmount(new_amount);
+}
+
+void ManagerController::removeProduct(string product_name) {
+    ProductModel target_product = ProductModel::getByName(db_connection_, product_name);
+    ProductModel::removeProduct(db_connection_, target_product.id());
+}
+
+vector<InventoryEntry> ManagerController::checkInventory() {
+    vector<ProductModel> products = ProductModel::getAll(db_connection_);
+    vector<InventoryEntry> inventory_table;
+    for(ProductModel& product : products) {
+        inventory_table.emplace_back(product.name(), product.amount(), product.price());
     }
-    if(result.num_rows() != 1) {
-        throw runtime_error("Error retrieving product with id=" + std::to_string(id) + "!\nNot enough rows returned by the database!");
-    }
-    // Processing the returned row
-    string name = result.rows()[0]["name"];
-    double price = std::stod(result.rows()[0]["price"]);
-    int amount = std::stoi(result.rows()[0]["amount"]);
-    return ManagerController(id, name, price, amount, db_connection);
-}
-
-ManagerController ManagerController::getByName(DatabaseConnection& db_connection, string name) {
-    string sql_statement = db_connection.
-        prepareStatement("SELECT * FROM Prodcuts WHERE name=?", "s", name.c_str());
-    QueryResults result = db_connection.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to retrieve product with name=" + name + "!\nDatabase error encountered: " + result.status_message());
-    }
-    if(result.num_rows() != 1) {
-        throw runtime_error("Error retrieving product with name=" + name + "!\nNot enough rows returned by the database!");
-    }
-    // Processing the returned row
-    int id = std::stoi(result.rows()[0]["id"]);
-    double price = std::stod(result.rows()[0]["price"]);
-    int amount = std::stoi(result.rows()[0]["amount"]);
-    return ManagerController(id, name, price, amount, db_connection);
-}
-
-ManagerController ManagerController::createProduct(
-    DatabaseConnection& db_connection,
-    string name,
-    double price,
-    int amount
-) {
-    string sql_statement = db_connection.prepareStatement(
-        "INSERT INTO Products (name, price, amount) VALUES (?, ?, ?)",
-        "sdi",
-        name.c_str(),
-        price,
-        amount
-    );
-    QueryResults result = db_connection.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to create new product!\nDatabase error encountered (code " + 
-            std::to_string(result.status_code()) + "): " + result.status_message());        
-    }
-    return ManagerController(db_connection.lastIdInserted(), name, price, amount, db_connection);
-}
-
-void ManagerController::removeProduct(DatabaseConnection& db_connection, int id) {
-    string sql_statement = db_connection.prepareStatement("DELETE FROM products WHERE id=?", "i", id);
-    QueryResults result = db_connection.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to delete product with id=" + std::to_string(id) +
-            "from the database!\nDatabase error encountered (code " +  std::to_string(result.status_code()) +
-                "): " + result.status_message());        
-    }
-}
-
-int ManagerController::id() {
-    return id_;
-}
-
-string ManagerController::name() {
-    return name_;
-}
-
-double ManagerController::price() {
-    return price_;
-}
-
-int ManagerController::amount() {
-    return amount_;
-}
-
-void ManagerController::updatePrice(double new_price) { 
-    string sql_statement = db_connection_.
-        prepareStatement("UPDATE Products SET price=? WHERE id=?", "di"
-        , new_price, id_);
-    QueryResults result = db_connection_.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to update product with id=" + std::to_string(id_) + 
-            "!\nDatabase error encountered (code " + std::to_string(result.status_code()) + 
-                "): " + result.status_message());
-    }
-    price_ = new_price;
-}
-
-void ManagerController::updateAmount(int new_amount) {
-    string sql_statement = db_connection_.
-        prepareStatement("UPDATE Products SET amount=? WHERE id=?", "ii"
-        , new_amount, id_);
-    QueryResults result = db_connection_.execute(sql_statement);
-    if(!result.success()) {
-        throw runtime_error("Failed to update product with id=" + std::to_string(id_) + 
-            "!\nDatabase error encountered (code " + std::to_string(result.status_code()) + 
-                "): " + result.status_message());
-    }
-    amount_ = new_amount;
+    return inventory_table;
 }
